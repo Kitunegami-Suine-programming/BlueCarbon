224a8389e9ec6e81f05d3266bd6ba048379d0d6e
Kitunegami-Suine-programming <maya.sekiguchi.0823@gmail.com>
2024-09-27 19:24:17 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let nav = document.querySelector("#navArea");
+let btn = document.querySelector(".batu");
+let mask = document.querySelector("#mask");
+ 
+btn.onclick = () => {
+    nav.classList.toggle("open")
+};
+
+jQuery(function() {
+    var pagetop = $('#pagetop');   
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {  //100pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500); //0.5秒かけてトップへ移動
+        return false;
+    });
+})
